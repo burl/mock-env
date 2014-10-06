d346cbaf963213ca76ca6f8fc6ceb52f0978dd49
burl <burl.bn@gmail.com>
2014-10-06 08:47:44 -0700

    added test to check for proper handling of conflicting arguments; fixed order of args in assert calls

--- test/unit.js
+++ test/unit.js
@@ -10,17 +10,18 @@ delete process.env.NOTSET;
 /* test known state against above */
 function checkUnmodifiedEnv() {
   it('should have expected values', function () {
-    assert.equal('foo', process.env.IS_SET);
-    assert.equal('bar', process.env.ALSO_SET);
+    assert.equal(process.env.IS_SET, 'foo');
+    assert.equal(process.env.ALSO_SET, 'bar');
   });
   it('should not have certain vars', function () {
-    assert.strictEqual(false, process.env.hasOwnProperty('NOTSET'));
-    assert.strictEqual(true, process.env.hasOwnProperty('ALSO_SET'));
+    assert.strictEqual(process.env.hasOwnProperty('NOTSET'), false);
+    assert.strictEqual(process.env.hasOwnProperty('ALSO_SET'), true);
   });
 }
 
 describe('mockEnv', function () {
   describe('unmodified environment', checkUnmodifiedEnv);
+
   describe('with changes', function () {
     var res;
 
@@ -35,20 +36,41 @@ describe('mockEnv', function () {
       }, {
         IS_SET: 'bada',
         NOTSET: 'bing'
-      }, ['ALSO_SET']);
+      }, [
+        'ALSO_SET'
+      ]);
     });
 
     it('should return the value from the callback', function () {
-      assert.equal('object', typeof res);
-      assert.strictEqual(true, res.hasOwnProperty('this_is_the_result'));
+      assert.equal(typeof res, 'object');
+      assert.strictEqual(res.hasOwnProperty('this_is_the_result'), true);
     });
 
     it('should have different values than before', function () {
-      assert.equal('bada', res.IS_SET);
-      assert.equal('bing', res.NOTSET);
-      assert.strictEqual(false, res.ALSO_SET_exists);
+      assert.equal(res.IS_SET, 'bada');
+      assert.equal(res.NOTSET, 'bing');
+      assert.strictEqual(res.ALSO_SET_exists, false);
     });
   });
+
+  describe('when arguments conflict', function() {
+    before(function () {
+      res = morph(function () {
+        return {
+          IS_SET: process.env.IS_SET,
+          NOTSET: process.env.NOTSET,
+          ALSO_SET_exists: process.env.hasOwnProperty('ALSO_SET')
+        };
+      }, {
+        IS_SET: 'bada',
+        NOTSET: 'bing'
+      }, [
+        'IS_SET'
+      ]);
+    });
+
+    describe('should preserve the original value', checkUnmodifiedEnv);
+  })
 
   describe('restored environment', checkUnmodifiedEnv);
 });
